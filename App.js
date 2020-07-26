0519343106c73deb03a9d2596bcb2ac76f39a040
uta630 <usuitkc.630@gmail.com>
2020-07-26 11:51:37 +0900

    ダミーデータをFlatList/ListItemで挿入

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import ListItem from "./components/ListItem";
+import articles from "./dummies/articles.json";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={index}
+      />
+    );
+  });
+
   return (
     <View style={styles.container}>
-      <ListItem
-        author="SampleNews"
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation"
-        imageUrl="https://picsum.photos/id/10/200/200"
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
