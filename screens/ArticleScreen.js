5790f6453abf75cc7bf5508f897d73f962d0ada6
uta630 <usuitkc.630@gmail.com>
2020-07-29 00:32:00 +0900

    storeに記事のaddとdelete

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview"
+import { useDispatch } from 'react-redux'
+import { addClip, deleteClip } from '../store/actions/user'
 
 const styles = StyleSheet.create({
   container: {
@@ -12,9 +14,24 @@ const styles = StyleSheet.create({
 export default ArticleScreen = (props) => {
   const { route } = props;
   const { article } = route.params
+  const dispatch = useDispatch()
 
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
